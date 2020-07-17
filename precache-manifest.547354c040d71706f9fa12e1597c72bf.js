b01e663eff56938e5157b3ae398741dcaebf1880
Makdoom Shaikh <makshaikh99@gmail.com>
2020-07-17 13:10:37 +0530

    Updates

---
+++ precache-manifest.547354c040d71706f9fa12e1597c72bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e75518f2209fe76615070d2ebd16630",
+    "url": "/foodieapp/index.html"
+  },
+  {
+    "revision": "929ea918f07fd1fdd617",
+    "url": "/foodieapp/static/css/main.4b10b193.chunk.css"
+  },
+  {
+    "revision": "e46aa116957d8c629634",
+    "url": "/foodieapp/static/js/2.e8e1c5c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foodieapp/static/js/2.e8e1c5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929ea918f07fd1fdd617",
+    "url": "/foodieapp/static/js/main.c5240219.chunk.js"
+  },
+  {
+    "revision": "146e555d1a550fdfb9be",
+    "url": "/foodieapp/static/js/runtime-main.365625dd.js"
+  }
+]);
